8d86f94bb03d489cb527618e402aad3f89d1f2ac
duttabikram <136303237+duttabikram@users.noreply.github.com>
2025-08-19 20:56:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-
+import sys
 __import__("pysqlite3")
 sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
 import streamlit as st
@@ -126,6 +126,7 @@ if api_key:
             st.write("Chat History:", session_history.messages)
 else:
     st.warning("Please enter the GROQ API Key")
+
 
 
 
